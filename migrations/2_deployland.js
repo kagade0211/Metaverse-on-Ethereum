dff879296ec60ccfc960deb30496a0b01a25cd5d
kagade0211 <kagadeakshay0211@gmail.com>
2022-06-01 08:37:41 +0530

    created tests in Land.test.js file

--- migrations/2_deployland.js
+++ migrations/2_deployland.js
@@ -1,9 +1,12 @@
 const Land = artifacts.require("Land");
 // requires  artifact of land smart contract i.e. json file that represent smart contract
-module.exports = function (deployer) {
+module.exports = function async (deployer) {
+ // use async as we are waiting until contract gets deployed
   deployer.deploy(Land);
+  // besides land smart contract we have to pass NAME,SYMBOL AND COST
   const NAME = 'DApp U Buildings'
   const SYMBOL = 'DUB'
+  // smart contracts only take in wei hence need to convert eth to wei using web3 library function
   const COST = web3.utils.toWei('1', 'ether')
   //wait until contract gets deployed
   await deployer.deploy(Land, NAME, SYMBOL, COST)
